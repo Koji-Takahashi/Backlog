54533c21026e8905c60957ce8303622b6953516c
Koji Takahashi <ktakahashi@toyo-group.co.jp>
2020-03-10 11:36:20 +0900

    Add elapsedTime

--- backlog/models.py
+++ backlog/models.py
@@ -6,6 +6,7 @@ class Issue(models.Model):
     name = models.CharField('name', max_length=255)
     issueKey = models.CharField('issueKey', max_length=255, blank=True)
     summary = models.CharField('summary', max_length=255, blank=True)
+    elapsedTime = models.TimeField('elapsedTime', blank=True)
 
     def __str__(self):
         return self.name
--- sample/settings.py
+++ sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=v(45sadw+0d!6_xf+l^4w(^yrt2u+31_y827t31+g80^mq-#_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.179.5.114']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
