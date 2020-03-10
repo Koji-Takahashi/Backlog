9b2077a70bab5bc20b29aee062414ad528729182
Koji Takahashi <ktakahashi@toyo-group.co.jp>
2020-03-10 13:08:43 +0900

    Alter elapsedTime

--- backlog/models.py
+++ backlog/models.py
@@ -6,7 +6,7 @@ class Issue(models.Model):
     name = models.CharField('name', max_length=255)
     issueKey = models.CharField('issueKey', max_length=255, blank=True)
     summary = models.CharField('summary', max_length=255, blank=True)
-    elapsedTime = models.TimeField('elapsedTime', blank=True)
+    elapsedTime = models.CharField('elapsedTime', max_length=255, blank=True)
 
     def __str__(self):
         return self.name
