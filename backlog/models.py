de74133722ba6ed809d131d1bc0abeeb5aaff14a
Koji Takahashi <ktakahashi@toyo-group.co.jp>
2020-03-10 14:41:49 +0900

    modification TimeStamp

--- backlog/models.py
+++ backlog/models.py
@@ -7,6 +7,7 @@ class Issue(models.Model):
     issueKey = models.CharField('issueKey', max_length=255, blank=True)
     summary = models.CharField('summary', max_length=255, blank=True)
     elapsedTime = models.CharField('elapsedTime', max_length=255, blank=True)
+    modificationTimeStamp = models.DateTimeField('modificationTimeStamp')
 
     def __str__(self):
         return self.name
